1178b5da5018a326e0a041e48679f6363d5e0e43
Steven Nguyen <stevenn38@hotmail.com>
2022-01-16 11:41:45 +1300

    Add customer images to hero section

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -2,6 +2,12 @@ import React from "react";
 
 // Images
 import hero from "../../images/hero.png";
+import customerOne from "../../images/customers/customer-1.jpg";
+import customerTwo from "../../images/customers/customer-2.jpg";
+import customerThree from "../../images/customers/customer-3.jpg";
+import customerFour from "../../images/customers/customer-4.jpg";
+import customerFive from "../../images/customers/customer-5.jpg";
+import customerSix from "../../images/customers/customer-6.jpg";
 
 // Styles
 import styles from "./Hero.module.css";
@@ -17,8 +23,7 @@ const Hero = () => {
                     <p className={styles.heroDescription}>
                         The smart 365-days-per-year food subscription that will
                         make you eat healthy again. Tailored to your personal
-                        tastes and nutritional needs. We have delivered 250,000+
-                        meals last year!
+                        tastes and nutritional needs.
                     </p>
                     <a href="#" className={`btn btn--full margin-right-sm`}>
                         Start eating well
@@ -26,6 +31,44 @@ const Hero = () => {
                     <a href="#" className={`btn btn--outline`}>
                         Learn more &darr;
                     </a>
+                    <div className={styles.deliveredMeals}>
+                        <div className={styles.deliveredImages}>
+                            <img
+                                src={customerOne}
+                                class={styles.customerImage}
+                                alt="Customer 1"
+                            />
+                            <img
+                                src={customerTwo}
+                                class={styles.customerImage}
+                                alt="Customer 2"
+                            />
+                            <img
+                                src={customerThree}
+                                class={styles.customerImage}
+                                alt="Customer 3"
+                            />
+                            <img
+                                src={customerFour}
+                                class={styles.customerImage}
+                                alt="Customer 4"
+                            />
+                            <img
+                                src={customerFive}
+                                class={styles.customerImage}
+                                alt="Customer 5"
+                            />
+                            <img
+                                src={customerSix}
+                                class={styles.customerImage}
+                                alt="Customer 6"
+                            />
+                        </div>
+                        <p class={styles.deliveredText}>
+                            <span>250,000+ </span>
+                            meals delivered last year!
+                        </p>
+                    </div>
                 </div>
                 <div className={styles.heroImageBox}>
                     <img
