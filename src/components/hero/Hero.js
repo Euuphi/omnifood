4b5b38f717b144e6b87430adeadf848364285c25
Steven Nguyen <stevenn38@hotmail.com>
2022-01-16 13:36:50 +1300

    Wrap hero into main element and adjust padding

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -14,71 +14,74 @@ import styles from "./Hero.module.css";
 
 const Hero = () => {
     return (
-        <section className={styles.sectionHero}>
-            <div className={styles.hero}>
-                <div className={styles.heroTextBox}>
-                    <h1 className={`heading-primary`}>
-                        A healthy meal delivered to your door, every single day
-                    </h1>
-                    <p className={styles.heroDescription}>
-                        The smart 365-days-per-year food subscription that will
-                        make you eat healthy again. Tailored to your personal
-                        tastes and nutritional needs.
-                    </p>
-                    <a href="#" className={`btn btn--full margin-right-sm`}>
-                        Start eating well
-                    </a>
-                    <a href="#" className={`btn btn--outline`}>
-                        Learn more &darr;
-                    </a>
-                    <div className={styles.deliveredMeals}>
-                        <div className={styles.deliveredImages}>
-                            <img
-                                src={customerOne}
-                                class={styles.customerImage}
-                                alt="Customer 1"
-                            />
-                            <img
-                                src={customerTwo}
-                                class={styles.customerImage}
-                                alt="Customer 2"
-                            />
-                            <img
-                                src={customerThree}
-                                class={styles.customerImage}
-                                alt="Customer 3"
-                            />
-                            <img
-                                src={customerFour}
-                                class={styles.customerImage}
-                                alt="Customer 4"
-                            />
-                            <img
-                                src={customerFive}
-                                class={styles.customerImage}
-                                alt="Customer 5"
-                            />
-                            <img
-                                src={customerSix}
-                                class={styles.customerImage}
-                                alt="Customer 6"
-                            />
-                        </div>
-                        <p class={styles.deliveredText}>
-                            <span>250,000+ </span>
-                            meals delivered last year!
+        <main>
+            <section className={styles.sectionHero}>
+                <div className={styles.hero}>
+                    <div className={styles.heroTextBox}>
+                        <h1 className={`heading-primary`}>
+                            A healthy meal delivered to your door, every single
+                            day
+                        </h1>
+                        <p className={styles.heroDescription}>
+                            The smart 365-days-per-year food subscription that
+                            will make you eat healthy again. Tailored to your
+                            personal tastes and nutritional needs.
                         </p>
+                        <a href="#" className={`btn btn--full margin-right-sm`}>
+                            Start eating well
+                        </a>
+                        <a href="#" className={`btn btn--outline`}>
+                            Learn more &darr;
+                        </a>
+                        <div className={styles.deliveredMeals}>
+                            <div className={styles.deliveredImages}>
+                                <img
+                                    src={customerOne}
+                                    class={styles.customerImage}
+                                    alt="Customer 1"
+                                />
+                                <img
+                                    src={customerTwo}
+                                    class={styles.customerImage}
+                                    alt="Customer 2"
+                                />
+                                <img
+                                    src={customerThree}
+                                    class={styles.customerImage}
+                                    alt="Customer 3"
+                                />
+                                <img
+                                    src={customerFour}
+                                    class={styles.customerImage}
+                                    alt="Customer 4"
+                                />
+                                <img
+                                    src={customerFive}
+                                    class={styles.customerImage}
+                                    alt="Customer 5"
+                                />
+                                <img
+                                    src={customerSix}
+                                    class={styles.customerImage}
+                                    alt="Customer 6"
+                                />
+                            </div>
+                            <p class={styles.deliveredText}>
+                                <span>250,000+ </span>
+                                meals delivered last year!
+                            </p>
+                        </div>
+                    </div>
+                    <div className={styles.heroImageBox}>
+                        <img
+                            src={hero}
+                            className={styles.heroImage}
+                            alt="Woman enjoying food, meals in storage container and food bowls on a table"
+                        />
                     </div>
                 </div>
-                <div className={styles.heroImageBox}>
-                    <img
-                        src={hero}
-                        className={styles.heroImage}
-                        alt="Woman enjoying food, meals in storage container and food bowls on a table"
-                    />
-                </div>
-            </div>
-        </section>
+            </section>
+        </main>
     );
 };
 
