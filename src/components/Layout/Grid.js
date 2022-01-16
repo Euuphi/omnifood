e1a02b1aae173ed01d6c0a793a497adb90be9729
Steven Nguyen <stevenn38@hotmail.com>
2022-01-17 12:13:31 +1300

    Improve Grid component reusability with more props
    - Add props for alignItems and justifyContent

--- src/components/Layout/Grid.js
+++ src/components/Layout/Grid.js
@@ -5,23 +5,27 @@ import React from "react";
  *
  * @param {number} col - Number of columns
  * @param {number} row - Number of rows
- * @param {number} gap - Value of gap property (in rem)
+ * @param {number} gap - Value of gap property (in rem) - Default: 9.6rem
  * @return {*}
  */
 
-const Grid = ({ children, col, row, gap }) => {
+const Grid = ({ children, col, row, gap, alignItems, justifyContent }) => {
     //Stylesheet
     const gridStyles = {
         display: "grid",
+        alignItems,
         gap: `${gap}rem`,
         gridTemplateColumns: `repeat(${col}, 1fr)`,
         gridTemplateRows: `repeat(${row}, 1fr)`,
+        justifyContent,
     };
 
     return <div style={gridStyles}>{children}</div>;
 };
 
 Grid.defaultProps = {
+    alignItems: "stretch",
+    justifyContent: "start",
     gap: 9.6,
     row: 1,
 };
