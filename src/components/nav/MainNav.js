367d40619b81a6b2e265d56c9eecb35ab0899a27
Steven Nguyen <stevenn38@hotmail.com>
2022-01-16 21:26:18 +1300

    Create and style temporary links for main nav bar

--- src/components/nav/MainNav.js
+++ src/components/nav/MainNav.js
@@ -1,14 +1,46 @@
 import React from "react";
 
+// Images
 import logo from "../../images/omnifood-logo.png";
 
+// Styles
 import styles from "./MainNav.module.css";
 
 const MainNav = () => {
     return (
-        <header class={styles.header}>
-            <img src={logo} class={styles.logo} alt="Omnifood logo" />
-            <nav class={styles.mainNav}>Nav</nav>
+        <header className={styles.header}>
+            <img src={logo} className={styles.logo} alt="Omnifood logo" />
+            <nav className={styles.mainNav}>
+                <ul className={styles.mainNavList}>
+                    <li className={styles.mainNavListItem}>
+                        <a className={styles.mainNavLink} href="#">
+                            Section 1
+                        </a>
+                    </li>
+                    <li className={styles.mainNavListItem}>
+                        <a className={styles.mainNavLink} href="#">
+                            Section 2
+                        </a>
+                    </li>
+                    <li className={styles.mainNavListItem}>
+                        <a className={styles.mainNavLink} href="#">
+                            Section 3
+                        </a>
+                    </li>
+                    <li className={styles.mainNavListItem}>
+                        <a className={styles.mainNavLink} href="#">
+                            Section 4
+                        </a>
+                    </li>
+                    <li className={styles.mainNavListItem}>
+                        <a
+                            className={`${styles.mainNavLink} ${styles.navCta}`}
+                            href="#">
+                            CTA
+                        </a>
+                    </li>
+                </ul>
+            </nav>
         </header>
     );
 };
