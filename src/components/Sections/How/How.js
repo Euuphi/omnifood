e8b0602403d8bc143c4dd928e339e8c99058a757
Steven Nguyen <stevenn38@hotmail.com>
2022-01-17 15:03:31 +1300

    Initial content dump for 'How It Works' section

--- src/components/Sections/How/How.js
+++ src/components/Sections/How/How.js
@@ -1,5 +1,8 @@
 import React from "react";
-import Container from "../Layout/Container";
+import Container from "../../Layout/Container";
+
+// Images
+import appScreenOne from "../../../images/app/app-screen-1.png";
 
 // Stylesheets
 import styles from "./How.module.css";
@@ -8,7 +11,34 @@ const How = () => {
     return (
         <secton className={styles.sectionHow}>
             <Container>
-                <h1 className={styles.subheading}>How it works</h1>
+                <span className={styles.subheading}>How it works</span>
+                {/* TODO: Extract to reusable component */}
+                <h2 className={`heading-secondary`}>
+                    Your daily dose of health in 3 simple steps
+                </h2>
+            </Container>
+            <Container>
+                <div className={styles.stepTextBox}>
+                    <p className={styles.stepNumber}>01</p>
+                    {/* TODO: Extract to reusable component */}
+                    <h3 className={`heading-tertiary`}>
+                        Tell us what you like (and what not)
+                    </h3>
+                    <p className={styles.stepDescription}>
+                        Never again waste time thinking about what to eat!
+                        Omnifood AI will create a 100% personalized weekly meal
+                        plan just for you. It makes sure you get all the
+                        nutrients and vitamins you need, no matter what diet you
+                        follow!
+                    </p>
+                </div>
+                <div className={styles.stepImageBox}>
+                    <img
+                        className={styles.stepImage}
+                        src={appScreenOne}
+                        alt="iPhone app preferences selection screen"
+                    />
+                </div>
             </Container>
         </secton>
     );
