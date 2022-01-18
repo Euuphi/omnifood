6a5a30ea181d188c082a1147d2bc0e8069b49e6e
Steven Nguyen <stevenn38@hotmail.com>
2022-01-18 16:03:25 +1300

    Add padding to Featured section and style heading

--- src/components/Sections/Featured/Featured.js
+++ src/components/Sections/Featured/Featured.js
@@ -3,6 +3,13 @@ import React from "react";
 // Stylesheets
 import styles from "./Featured.module.css";
 
+// Images - Logos
+import businessInsider from "../../../images/logos/business-insider.png";
+import forbes from "../../../images/logos/forbes.png";
+import newYorkTimes from "../../../images/logos/the-new-york-times.png";
+import techCrunch from "../../../images/logos/techcrunch.png";
+import usaToday from "../../../images/logos/usa-today.png";
+
 import Container from "../../Layout/Container";
 
 const Featured = () => {
@@ -10,6 +17,13 @@ const Featured = () => {
         <section className={styles.sectionFeatured}>
             <Container>
                 <h2 className={styles.headingFeaturedIn}>As featured in</h2>
+                <div className={styles.logos}>
+                    <img src={techCrunch} alt="Techcrunch logo" />
+                    <img src={businessInsider} alt="Business Insider logo" />
+                    <img src={newYorkTimes} alt="The New York Times logo" />
+                    <img src={forbes} alt="Forbes logo" />
+                    <img src={usaToday} alt="USA Today logo" />
+                </div>
             </Container>
         </section>
     );
