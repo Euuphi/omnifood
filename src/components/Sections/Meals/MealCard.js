fba5b99fc2dd52be97b2175b02f54ffb15d42aff
Steven Nguyen <stevenn38@hotmail.com>
2022-01-19 10:27:35 +1300

    Finish MealCard styling

--- src/components/Sections/Meals/MealCard.js
+++ src/components/Sections/Meals/MealCard.js
@@ -2,7 +2,6 @@ import React from "react";
 
 // Images
 import mealOne from "../../../images/meals/meal-1.jpg";
-import mealTwo from "../../../images/meals/meal-2.jpg";
 
 // Icons
 import { FlameOutline, RestaurantOutline, StarOutline } from "react-ionicons";
@@ -20,28 +19,35 @@ const MealCard = () => {
                 src={mealOne}
                 alt="Japanese Gyozas"
             />
-            <span className={styles.tag}>Vegetartian</span>
-            <p className={styles.mealTitle}>Japanese Gyozas</p>
-            <ul className={styles.mealAttributeList}>
-                <li className={styles.mealAttribute}>
-                    <FlameOutline color={colorPrimary} height="2.4rem" />
-                    <span>
-                        <strong>650</strong> calories
-                    </span>
-                </li>
-                <li className={styles.mealAttribute}>
-                    <RestaurantOutline color={colorPrimary} height="2.4rem" />
-                    <span>
-                        NutriScore &reg; <strong>74</strong>
-                    </span>
-                </li>
-                <li className={styles.mealAttribute}>
-                    <StarOutline color={colorPrimary} height="2.4rem" />
-                    <span>
-                        <strong>4.9</strong> rating (537)
-                    </span>
-                </li>
-            </ul>
+            <div className={styles.mealContent}>
+                <div className={styles.tagList}>
+                    <span className={styles.tag}>Vegetartian</span>
+                </div>
+                <p className={styles.mealTitle}>Japanese Gyozas</p>
+                <ul className={styles.mealAttributeList}>
+                    <li className={styles.mealAttribute}>
+                        <FlameOutline color={colorPrimary} height="2.4rem" />
+                        <span>
+                            <strong>650</strong> calories
+                        </span>
+                    </li>
+                    <li className={styles.mealAttribute}>
+                        <RestaurantOutline
+                            color={colorPrimary}
+                            height="2.4rem"
+                        />
+                        <span>
+                            NutriScore &reg; <strong>74</strong>
+                        </span>
+                    </li>
+                    <li className={styles.mealAttribute}>
+                        <StarOutline color={colorPrimary} height="2.4rem" />
+                        <span>
+                            <strong>4.9</strong> rating (537)
+                        </span>
+                    </li>
+                </ul>
+            </div>
         </div>
     );
 };
