f9db06811f2b1e5a7f3199eb6ac58ad515dc6399
Steven Nguyen <stevenn38@hotmail.com>
2022-01-18 20:37:29 +1300

    Create MealCard component

---
+++ src/components/Sections/Meals/MealCard.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+// Stylesheets
+import styles from "./MealCard.module.css";
+
+const MealCard = () => {
+    return <div className={styles.meal}>Meal Card</div>;
+};
+
+export default MealCard;
--- src/components/Sections/Meals/Meals.js
+++ src/components/Sections/Meals/Meals.js
@@ -4,7 +4,10 @@ import React from "react";
 import styles from "./Meals.module.css";
 
 // Components
+import Container from "../../Layout/Container";
+import Grid from "../../Layout/Grid";
 import SectionHeading from "../../Headings/SectionHeading";
+import MealCard from "./MealCard";
 
 const Meals = () => {
     return (
@@ -13,6 +16,11 @@ const Meals = () => {
                 subheading="Meals"
                 secondaryHeading="Omnifood AI chooses from 5,000+ recipes"
             />
+            <Container>
+                <Grid col={3}>
+                    <MealCard />
+                </Grid>
+            </Container>
         </section>
     );
 };
