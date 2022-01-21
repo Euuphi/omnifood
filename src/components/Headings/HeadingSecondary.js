d3eaa1ad4b2c6d008e9296601290da81620a10ed
Steven Nguyen <stevenn38@hotmail.com>
2022-01-21 22:34:38 +1300

    Add className prop to HeadingSecondary

--- src/components/Headings/HeadingSecondary.js
+++ src/components/Headings/HeadingSecondary.js
@@ -3,8 +3,19 @@ import React from "react";
 // Stylesheets
 import styles from "./Headings.module.css";
 
-const HeadingSecondary = ({ children }) => {
-    return <h2 className={styles.headingSecondary}>{children}</h2>;
+/**
+ * Create basic secondary heading
+ *
+ * @param {string} className - Add additoinal classes for styling
+ * @return {*}
+ */
+
+const HeadingSecondary = ({ children, className }) => {
+    return (
+        <h2 className={`${styles.headingSecondary} ${className}`}>
+            {children}
+        </h2>
+    );
 };
 
 export default HeadingSecondary;
