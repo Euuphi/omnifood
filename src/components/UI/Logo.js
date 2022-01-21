fb0a61563d9b1548781fdbd486a2ba28f0bd78f3
Steven Nguyen <stevenn38@hotmail.com>
2022-01-22 11:34:58 +1300

    Wrap logo in anchor tag

--- src/components/UI/Logo.js
+++ src/components/UI/Logo.js
@@ -7,7 +7,11 @@ import logo from "../../images/omnifood-logo.png";
 import styles from "./Logo.module.css";
 
 const Logo = () => {
-    return <img src={logo} className={styles.logo} alt="Omnifood logo" />;
+    return (
+        <a href="#">
+            <img src={logo} className={styles.logo} alt="Omnifood logo" />
+        </a>
+    );
 };
 
 export default Logo;
