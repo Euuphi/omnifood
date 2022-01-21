35d0d728c06381ed7fa343e2bff664725c6d74ae
Steven Nguyen <stevenn38@hotmail.com>
2022-01-21 22:35:03 +1300

    Add and style CTA section text content

--- src/components/Sections/CTA/CTA.js
+++ src/components/Sections/CTA/CTA.js
@@ -6,6 +6,7 @@ import styles from "./CTA.module.css";
 //Components
 import Container from "../../Layout/Container";
 import Grid from "../../Layout/Grid";
+import HeadingSecondary from "../../Headings/HeadingSecondary";
 
 const CTA = () => {
     return (
@@ -13,7 +14,16 @@ const CTA = () => {
             <Container marginBottom="9.6rem">
                 <div className={styles.cta}>
                     <Grid col="2fr 1fr" gap="0">
-                        <div className={styles.ctaTextBox}>CTA</div>
+                        <div className={styles.ctaTextBox}>
+                            <HeadingSecondary className={styles.ctaHeading}>
+                                Get your first meal for free!
+                            </HeadingSecondary>
+                            <p className={styles.ctaText}>
+                                Healthy, tasty and hassle-free meals are waiting
+                                for you. Start eating well today. You can cancel
+                                or pause anytime. And the first meal is on us!
+                            </p>
+                        </div>
                         <div
                             className={styles.ctaImageBox}
                             role="img"
