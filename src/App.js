3084e73c4127168992b3624ef3962a1c114aa9bd
Steven Nguyen <stevenn38@hotmail.com>
2022-01-22 11:10:46 +1300

    Adjust <main> element placement for page

--- src/App.js
+++ src/App.js
@@ -9,18 +9,22 @@ import Meals from "./components/Sections/Meals/Meals";
 import TestimonialsSection from "./components/Sections/Testimonials/TestimonialsSection";
 import Pricing from "./components/Sections/Pricing/Pricing";
 import CTA from "./components/Sections/CTA/CTA";
+import Footer from "./components/Footer/Footer";
 
 function App() {
     return (
         <Fragment>
             <MainNav />
-            <Hero />
-            <Featured />
-            <How />
-            <Meals />
-            <TestimonialsSection />
-            <Pricing />
-            <CTA />
+            <main>
+                <Hero />
+                <Featured />
+                <How />
+                <Meals />
+                <TestimonialsSection />
+                <Pricing />
+                <CTA />
+            </main>
+            <Footer />
         </Fragment>
     );
 }
--- src/components/Sections/Hero/Hero.js
+++ src/components/Sections/Hero/Hero.js
@@ -20,83 +20,81 @@ import Button from "../../UI/Button";
 
 const Hero = () => {
     return (
-        <main>
-            <section className={styles.sectionHero}>
-                <Container maxWidth="130rem">
-                    <Grid col={2} alignItems="center">
-                        <div className={styles.heroTextBox}>
-                            <HeadingPrimary>
-                                A healthy meal delivered to your door, every
-                                single day
-                            </HeadingPrimary>
-                            <p className={styles.heroDescription}>
-                                The smart 365-days-per-year food subscription
-                                that will make you eat healthy again. Tailored
-                                to your personal tastes and nutritional needs.
-                            </p>
-                            {/* TODO: Extract into reusable component */}
-                            <Button
-                                href="#"
-                                className="margin-right-sm"
-                                text="Start eating well"
-                                preStyle="full"
-                            />
-                            <Button
-                                href="#"
-                                className="margin-right-sm"
-                                text="Learn more &darr;"
-                                preStyle="outline"
-                            />
-                            <div className={styles.deliveredMeals}>
-                                <div className={styles.deliveredImages}>
-                                    <img
-                                        className={styles.customerImage}
-                                        src={customerOne}
-                                        alt="Customer 1"
-                                    />
-                                    <img
-                                        className={styles.customerImage}
-                                        src={customerTwo}
-                                        alt="Customer 2"
-                                    />
-                                    <img
-                                        className={styles.customerImage}
-                                        src={customerThree}
-                                        alt="Customer 3"
-                                    />
-                                    <img
-                                        className={styles.customerImage}
-                                        src={customerFour}
-                                        alt="Customer 4"
-                                    />
-                                    <img
-                                        className={styles.customerImage}
-                                        src={customerFive}
-                                        alt="Customer 5"
-                                    />
-                                    <img
-                                        className={styles.customerImage}
-                                        src={customerSix}
-                                        alt="Customer 6"
-                                    />
-                                </div>
-                                <p className={styles.deliveredText}>
-                                    <span>250,000+ </span>
-                                    meals delivered last year!
-                                </p>
+        <section className={styles.sectionHero}>
+            <Container maxWidth="130rem">
+                <Grid col={2} alignItems="center">
+                    <div className={styles.heroTextBox}>
+                        <HeadingPrimary>
+                            A healthy meal delivered to your door, every single
+                            day
+                        </HeadingPrimary>
+                        <p className={styles.heroDescription}>
+                            The smart 365-days-per-year food subscription that
+                            will make you eat healthy again. Tailored to your
+                            personal tastes and nutritional needs.
+                        </p>
+                        {/* TODO: Extract into reusable component */}
+                        <Button
+                            href="#"
+                            className="margin-right-sm"
+                            text="Start eating well"
+                            preStyle="full"
+                        />
+                        <Button
+                            href="#"
+                            className="margin-right-sm"
+                            text="Learn more &darr;"
+                            preStyle="outline"
+                        />
+                        <div className={styles.deliveredMeals}>
+                            <div className={styles.deliveredImages}>
+                                <img
+                                    className={styles.customerImage}
+                                    src={customerOne}
+                                    alt="Customer 1"
+                                />
+                                <img
+                                    className={styles.customerImage}
+                                    src={customerTwo}
+                                    alt="Customer 2"
+                                />
+                                <img
+                                    className={styles.customerImage}
+                                    src={customerThree}
+                                    alt="Customer 3"
+                                />
+                                <img
+                                    className={styles.customerImage}
+                                    src={customerFour}
+                                    alt="Customer 4"
+                                />
+                                <img
+                                    className={styles.customerImage}
+                                    src={customerFive}
+                                    alt="Customer 5"
+                                />
+                                <img
+                                    className={styles.customerImage}
+                                    src={customerSix}
+                                    alt="Customer 6"
+                                />
                             </div>
+                            <p className={styles.deliveredText}>
+                                <span>250,000+ </span>
+                                meals delivered last year!
+                            </p>
                         </div>
-                        <div className={styles.heroImageBox}>
-                            <img
-                                src={hero}
-                                className={styles.heroImage}
-                                alt="Woman enjoying food, meals in storage container and food bowls on a table"
-                            />
-                        </div>
-                    </Grid>
-                </Container>
-            </section>
-        </main>
+                    </div>
+                    <div className={styles.heroImageBox}>
+                        <img
+                            src={hero}
+                            className={styles.heroImage}
+                            alt="Woman enjoying food, meals in storage container and food bowls on a table"
+                        />
+                    </div>
+                </Grid>
+            </Container>
+        </section>
     );
 };
 
